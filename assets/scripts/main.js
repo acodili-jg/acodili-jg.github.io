0f79a6fe18a75582a8929995b21c737b68431022
acodili-jg <acodili.jackgabriel@gmail.com>
2023-10-17 23:00:03 +0800

    Prefix local storage key for themes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 var themeOptionNodeList = document.getElementsByName("themes");
 
 export function init() {
-  var loadedTheme = window.localStorage.getItem("theme");
+  var loadedTheme = localStorage.getItem("acodili-jg.github.io/theme");
 
   switch (loadedTheme) {
     case "dark":
@@ -18,7 +18,7 @@ export function init() {
     themeOption.addEventListener("change", () => {
       var selectedTheme = themeOption.value;
       document.documentElement.setAttribute("data-theme", selectedTheme);
-      window.localStorage.setItem("theme", selectedTheme);
+      localStorage.setItem("acodili-jg.github.io/theme", selectedTheme);
     });
 
     themeOption.checked = themeOption.value == loadedTheme;
